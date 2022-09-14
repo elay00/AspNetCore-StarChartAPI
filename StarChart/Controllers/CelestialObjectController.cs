38a2ff5d1f920f7f730d3920611164b73ae58426
Elie <ebrakha@github.com>
2022-09-14 15:19:29 +0200

    [EBR](Add) Create The CelestialObjectController Class

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    /// <summary>
+    /// CeletiaObjectController classe
+    /// </summary>
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : Controller
+    {
+        ApplicationDbContext _context;
+
+        /// <summary>
+        /// Initializes a new instance <see cref="CelestialObjectController"/>
+        /// </summary>
+        /// <param name="context">the database context</param>
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Index Action
+        /// </summary>
+        /// <returns>The IActionResult View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
