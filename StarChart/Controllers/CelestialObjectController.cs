90ef7135d9f0189dd6c6907ef734744aa690b3da
Elie <ebrakha@github.com>
2022-09-14 15:42:36 +0200

    [EBR](Add) Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -24,5 +26,24 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Gets Celestian object by Id
+        /// </summary>
+        /// <param name="id">the Id of the celestian object to get</param>
+        /// <returns>an action result</returns>
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            CelestialObject celestianObject = null;
+            if (celestianObject.Id != id)
+            {
+                return NotFound("The object is not found with the provided Id");
+            }
+            else
+            {
+                celestianObject.Satellites = new System.Collections.Generic.List<CelestialObject> { celestianObject };
+                return Ok();
+            }
+        }
     }
 }
